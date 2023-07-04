d767b867a332ca051226fa92dfead3a86d10704e
lyoben <lyoben92@gmail.com>
2023-07-04 21:58:39 +0800

    first commit

---
+++ cka_exam_quickstart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source <(kubectl completion bash)
+echo "source <(kubectl completion bash)" >> ~/.bashrc
+
+alias k=kubectl
+complete -o default -F __start_kubectl k
+
+export now="--force --grace-period 0"
+export do="--dry-run=client -o yaml" 
+export kns="kubectl config set-context --curent --namespace"
+export kgs="kubectl config get-contexts"
